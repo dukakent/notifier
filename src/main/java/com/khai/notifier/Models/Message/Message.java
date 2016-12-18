9396313ebb73a60955b997693ad733d14a80098e
Inokentii Duka <fockewulf3@gmail.com>
2016-12-18 16:18:10 +0200

    comments added

--- src/main/java/com/khai/notifier/Models/Message/Message.java
+++ src/main/java/com/khai/notifier/Models/Message/Message.java
@@ -1,22 +1,32 @@
 package com.khai.notifier.Models.Message;
 
 /**
- * Created by Yermakov Vladislav on 12/17/2016.
+ * Message model, consists of subject and text
  */
 public class Message {
 
     private String subject;
     private String text;
 
+    /**
+     * @param subject message subject (title, header)
+     * @param text message main text (body)
+     */
     public Message(String subject, String text) {
         this.subject = subject;
         this.text = text;
     }
 
+    /**
+     * @return message's subject
+     */
     public String getSubject() {
         return subject;
     }
 
+    /**
+     * @return message's body
+     */
     public String getText() {
         return text;
     }
--- src/main/java/com/khai/notifier/Models/User/UserFactory.java
+++ src/main/java/com/khai/notifier/Models/User/UserFactory.java
@@ -3,11 +3,24 @@ package com.khai.notifier.Models.User;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Users' factory responsible for creating instances of Users
+ */
 public class UserFactory {
+    /**
+     * Creates single User instance
+     * @param record String array of user attributes
+     * @return new User instance
+     */
     private static User create(String[] record) {
         return new User(record);
     }
 
+    /**
+     * Creates array of User instances
+     * @param records 2D Array contains many users' attributes
+     * @return List of user instances
+     */
     public static List<User> createList(String[][] records) {
         List<User> list = new ArrayList<User>();
 
