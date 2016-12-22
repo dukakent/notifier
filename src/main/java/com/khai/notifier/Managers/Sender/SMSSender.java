12b2007a4fe19b255cb28e0c0e7658d88e1aca6c
Yermakov Vladislav <yermakov.vladislav@yandex.ru>
2016-12-22 22:56:45 +0200

    Sender reviewed. (#7)
    Constant section added.
    Code cleaned and markup'ed.

--- src/main/java/com/khai/notifier/Managers/Sender/SMSSender.java
+++ src/main/java/com/khai/notifier/Managers/Sender/SMSSender.java
@@ -10,6 +10,9 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 /**
  * SMSSender uses HTTP POST method to use sms-fly.com API
@@ -17,22 +20,47 @@ import org.apache.http.impl.client.HttpClientBuilder;
  */
 public class SMSSender extends Sender {
 
+    // Constants Section
+    private static String HOST_NAME                          = "sms-fly.com";
+    private static String API_ENDPOINT                       = "http://sms-fly.com/api/api.noai.php";
+    private static String REQUEST_HEADER_HOST_KEY            = "Host:";
+    private static String REQUEST_HEADER_AUTH_KEY            = "Authorization:";
+    private static String REQUEST_HEADER_AUTH_VALUE          = "Basic";
+    private static String REQUEST_HEADER_CONTENT_TYPE_KEY    = "Content-type:" ;
+    private static String REQUEST_HEADER_CONTENT_TYPE_VALUE  = "text/xml";
+    private static String REQUEST_HEADER_CONTENT_LENGTH_KEY  = "Content-Length:";
+    private static String REQUEST_BODY_HEADER                = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+    private static String REQUEST_BODY_RECIPIENT_PLACEHOLDER = "USERPHONE";
+    private static String REQUEST_BODY_BODY_PLACEHOLDER      = "REQESTBODY";
+    private static String REQUEST_BODY_OPERATION_SEND        = "<operation>SENDSMS</operation>";
+    private static String REQUEST_BODY_OPERATION_SEND_BODY   =
+            "<request>" +
+                "<message start_time=\"AUTO\" end_time=\"AUTO\" lifetime=\"4\" desc=\"description\">" +
+                    "<recipient>"  + REQUEST_BODY_RECIPIENT_PLACEHOLDER + "</recipient>" +
+                    "<body>"       + REQUEST_BODY_BODY_PLACEHOLDER      + "</body>" +
+                "</message>" +
+            "</request>";
+
+
+    // Properties
     private String username;
     private String password;
-    private static String HOST_NAME = "sms-fly.com";
 
+
+    // Init
     public SMSSender(String user, String pass) {
         this.username = user;
         this.password = pass;
     }
 
+    // Functions
     @Override
     public void send(Message message, String phone) {
 
         try {
             sendPost(message.getText(), phone);
         } catch (Exception e) {
-            Output.error("Error sending sms to user with phone: " + phone);
+            Output.error("Error sending sms to user with phone: " + user.getPhone() + ".\nDescription: " + e.getLocalizedMessage() + ".\n");
         }
     }
 
@@ -41,37 +69,51 @@ public class SMSSender extends Sender {
      *
      * @param body  Message body
      * @param phone User phone number
-     * @throws Exception
+     * @throws Exception Exception could be thrown in case of http post request/response
      */
     private void sendPost(String body, String phone) throws Exception {
 
         byte[] encodedAuthorizationHeaderValue = Base64.encodeBase64((username + ":" + password).getBytes());
-        String authorizationHeaderValue = new String(encodedAuthorizationHeaderValue);
-        String url = "http://sms-fly.com/api/api.noai.php";
-        String xml =
-                "<?xml version=\"1.0\" encoding=\"utf-8\"?><request>\n" +
-                        "<operation>SENDSMS</operation>\n" +
-                        "<message start_time=\"AUTO\" end_time=\"AUTO\" lifetime=\"4\" desc=\"description\">" +
-                        "<recipient>" + phone + "</recipient>\n" +
-                        "<body>" + body + "</body>\n" +
-                        "</message>\n" +
-                        "</request>";
+        String authorizationHeaderValue        = new String(encodedAuthorizationHeaderValue);
+
+        String url = API_ENDPOINT;
+
+        String responseBody = REQUEST_BODY_OPERATION_SEND_BODY;
+        responseBody        = responseBody.replace(REQUEST_BODY_RECIPIENT_PLACEHOLDER, phone);
+        responseBody        = responseBody.replace(REQUEST_BODY_BODY_PLACEHOLDER,      body);
+
+        String xml = REQUEST_BODY_HEADER + REQUEST_BODY_OPERATION_SEND + responseBody;
 
         HttpEntity entity = new ByteArrayEntity(xml.getBytes("UTF-8"));
         HttpClient client = HttpClientBuilder.create().build();
-        HttpPost post = new HttpPost(url);
+        HttpPost post     = new HttpPost(url);
 
         // add headers
-        post.setHeader("Host:", HOST_NAME);
-        post.setHeader("Authorization:", "Basic " + authorizationHeaderValue);
-        post.setHeader("Content-type:", "text/xml");
-        post.setHeader("Content-Length:", Integer.toString(xml.length()));
-
+        post.setHeader(REQUEST_HEADER_HOST_KEY,           HOST_NAME);
+        post.setHeader(REQUEST_HEADER_AUTH_KEY,           REQUEST_HEADER_AUTH_VALUE + " " + authorizationHeaderValue);
+        post.setHeader(REQUEST_HEADER_CONTENT_TYPE_KEY,   REQUEST_HEADER_CONTENT_TYPE_VALUE);
+        post.setHeader(REQUEST_HEADER_CONTENT_LENGTH_KEY, Integer.toString(xml.length()));
         post.setEntity(entity);
 
-        HttpResponse response = client.execute(post);
-        Output.info("\nSending 'POST' request to URL : " + url);
+        Output.info("Sending 'POST' request to URL : " + url);
         Output.info("Post parameters : " + post.getEntity());
-        Output.info("Response Code : " + response.getStatusLine().getStatusCode());
+
+        // get response
+        HttpResponse response     = client.execute(post);
+        HttpEntity responseEntity = response.getEntity();
+        String content            = EntityUtils.toString(responseEntity);
+
+        // TODO: response body has to be managed
+        int statusCode = response.getStatusLine().getStatusCode();
+        switch (statusCode) {
+
+            case 200:
+                Output.success("SMS request sent successfully.");
+                break;
+
+            default:
+                Output.error("SMS request error.\nResponse Body:" + content);
+                break;
+        }
     }
 }
